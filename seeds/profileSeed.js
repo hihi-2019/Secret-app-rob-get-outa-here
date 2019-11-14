e237a6064b7f1c086f8da9e738451265b7cfab06
student <student@eda-bc-04.localdomain>
2019-11-14 18:23:02 +1300

    setup database

---
+++ seeds/profileSeed.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('profiles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('profiles').insert([
+        {profile_id: 1, name: 'Ollie', age: '18', profile_img:'profilepic.jpg', favourite_quote:'hasbfkab glhsdbfgja', banner_img: '#', bio:'dfghjkdfghj'}
+      ]);
+    });
+};
