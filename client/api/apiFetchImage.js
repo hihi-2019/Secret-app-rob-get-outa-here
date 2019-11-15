a8878c98a7e42fdb8f6925b8888b8e76a11e9b46
Student <student@eda-bc-05.localdomain>
2019-11-15 14:40:52 +1300

    added additional photo refresh

--- client/api/apiFetchImage.js
+++ client/api/apiFetchImage.js
@@ -2,6 +2,9 @@ import request from 'superagent'
 
 const unsplash = 'https://source.unsplash.com/450x450/?'
 
+const recentPhotos = 'https://source.unsplash.com/800x450/?goat'
+
+
 export function getProfileImage() {
   return request
   .get(unsplash + 'elderly')
@@ -9,5 +12,7 @@ export function getProfileImage() {
 }
 
 export function getRecentPhotos () {
-  return request.get(unsplash).then(res => res)
+  return request
+  .get(recentPhotos)
+  .then(res => res)
 }
--- client/components/RecentPhotos.jsx
+++ client/components/RecentPhotos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import request from 'superagent'
+import { getRecentPhotos } from '../api/apiFetchImage'
 
 //Populated with images from external photography API(s)
 class RecentPhotos extends React.Component {
@@ -8,18 +9,50 @@ class RecentPhotos extends React.Component {
     this.state = {
       images: []
     }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     request.get('/api/recent-images/1')
       .then(res => {
         console.log(res.body)
-
         this.setState({
-          images: res.body
+          images: res.body.map(image => `/images/${image.img_filename}`)
         })
       })
   }
+
+  handleClick(event) {
+    event.preventDefault()
+    this.setState({ images: [] }, () => {
+      for (let i = 0; i < 9; i++) {
+
+        setTimeout(() => {
+          getRecentPhotos()
+          .then(res => {
+            let ts = Math.round((new Date()).getTime())
+            const newState = [...this.state.images]
+            newState.push(res.req.url)
+            this.setState({
+              images: newState
+            })
+          })
+        }, i*3500)
+        
+        // getRecentPhotos()
+        //   .then(res => {
+        //     let ts = Math.round((new Date()).getTime())
+        //     const newState = [...this.state.images]
+        //     newState.push(res.req.url + '?t=' + ts)
+        //     this.setState({
+        //       images: newState
+        //     })
+        //   })
+      }
+    })
+  }
+
+
 
   render() {
 
@@ -29,20 +62,20 @@ class RecentPhotos extends React.Component {
 
         <div className="py-4 px-4">
           <div className="d-flex align-items-center justify-content-between mb-3">
-            <h5 className="mb-0">Recent photos</h5><a href="#" className="btn btn-link text-muted">Show all</a>
+            <h5 className="mb-0">Recent photos</h5><button href="#" className="btn btn-outline-dark btn-sm" onClick={this.handleClick}>Randomise Photos</button>
           </div>
           <div className="row">
             {this.state.images && (
-              
-                this.state.images.map(image => {
-                  return( 
-               
+
+              this.state.images.map(image => {
+                return (
+
                   <div className="col-lg-6 mb-2 pr-lg-1">
-                    <img src={`./images/${image.img_filename}`} alt="" className="img-fluid rounded shadow-sm" />
-                    </div> 
-                  )
-                })
-              
+                    <img src={image} alt="" className="img-fluid rounded shadow-sm" />
+                  </div>
+                )
+              })
+
             )}
           </div>
         </div>
